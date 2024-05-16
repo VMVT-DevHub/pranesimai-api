107ae29e674109c39955309514630ea0feb149e0
Pranciskus Ambrazas <p.ambrazas@gmail.com>
2024-05-16 11:29:20 +0300

    sharepoint fields

---
+++ database/migrations/20240516112011_sharepointField.js
@@ -0,0 +1,31 @@
+const { commonFields, schema } = require('../common');
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema
+    .withSchema(schema)
+    .alterTable('surveys', (table) => {
+      table.string('spList');
+    })
+    .alterTable('questions', (table) => {
+      table.string('spField');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema
+    .withSchema(schema)
+    .alterTable('surveys', (table) => {
+      table.dropColumn('spList');
+    })
+    .alterTable('questions', (table) => {
+      table.dropColumn('spField');
+    });
+};
