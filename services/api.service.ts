0be033e849ca24f58e683791461b56585b59c131
Mantrimas Tadijošaitis <mantrimas.tadijosaitis@vmvt.lt>
2026-05-06 14:46:15 +0300

    remove openapi routes from test/production configuration

--- services/api.service.ts
+++ services/api.service.ts
@@ -16,22 +16,6 @@ export enum RestrictionType {
   SESSION = 'SESSION',
 }
 
-const openApiRoutes =
-  process.env.NODE_ENV === 'production'
-    ? []
-    : [
-        {
-          path: '/openapi',
-          authorization: false,
-          authentication: false,
-          aliases: {
-            'GET /openapi.json': 'openapi.generateDocs', // swagger scheme
-            'GET /ui': 'openapi.ui', // ui
-            'GET /assets/:file': 'openapi.assets', // js/css files
-          },
-        },
-      ];
-
 @Service({
   name: 'api',
   mixins: [ApiGateway],
@@ -57,7 +41,6 @@ const openApiRoutes =
     },
 
     routes: [
-      ...openApiRoutes,
       {
         path: '/',
         whitelist: [
